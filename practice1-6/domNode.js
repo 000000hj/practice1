2b82c6ea4291aa45b303cb5feb3be57d1eb63599
000000hj <heoynju50@gmail.com>
2022-10-17 15:08:45 +0900

    domf

--- practice1-6/domNode.js
+++ practice1-6/domNode.js
@@ -1,10 +1,29 @@
 function newRegister(){
     var newP=document.createElement("p");
+
+    var delBtn=document.createElement("span");
+    var deltext=document.createTextNode("X");
+    delBtn.setAttribute("class","del");
+    delBtn.appendChild(deltext);
+    newP.appendChild(delBtn);
+
     var userName=document.querySelector('#userName');
     var newText=document.createTextNode(userName.value);
     newP.appendChild(newText);
     var nameList=document.querySelector('#nameList');
+   
     nameList.appendChild(newP);
     userName.value="";
+
+    var removeBtn=document.querySelectorAll(".del");
+    for(var i=0;i<removeBtn.length;i++)
+{
+    removeBtn[i].addEventListener("click",function(){
+        if(this.parentNode.parentNode)
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    });
 }
-var firstDel = document.querySelectorAll(".del")[0];
+
+}
+
+
